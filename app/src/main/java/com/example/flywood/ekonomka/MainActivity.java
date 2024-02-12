da77e22ef43942f87cb3ce496bf6027cc8ee6452
Алексей Компанец <trueflywood@gmail.com>
2024-02-12 21:31:53 +0300

    menu

--- app/src/main/java/com/example/flywood/ekonomka/MainActivity.java
+++ app/src/main/java/com/example/flywood/ekonomka/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.flywood.ekonomka;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private  Menu rightMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +38,24 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("FLYWOOD", "test");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
+        navigationView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i("FLYWOOD", "TEST CLICK");
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home,
-                R.id.nav_slideshow,
                 R.id.nav_receipt,
                 R.id.nav_goods,
                 R.id.nav_about
@@ -52,12 +65,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+
+            @Override
+            public void onDestinationChanged(
+                    @NonNull NavController navController,
+                    @NonNull NavDestination navDestination,
+                    @Nullable Bundle bundle
+            ) {
+
+                if (navDestination.getId() == R.id.nav_receipt) {
+                    Log.i("FLYWOOD", "TEST CLICK 5");
+                }
+
+
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // TODO добавлять в зависимостиот экрана
+
+        this.rightMenu = menu;
+//        getMenuInflater().inflate(R.menu.main, menu);
+//
+//        menu.clear();
+
+
         return true;
     }
 
