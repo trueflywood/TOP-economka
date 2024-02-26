010d3cb6f2c43e1580e3fa35a9b65a18d3b44d0b
Алексей Компанец <trueflywood@gmail.com>
2024-02-27 01:10:45 +0300

    Сканирование штрихкода

--- app/src/main/java/com/example/flywood/ekonomka/MainActivity.java
+++ app/src/main/java/com/example/flywood/ekonomka/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.flywood.ekonomka;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,7 +18,11 @@ import com.example.flywood.ekonomka.data.services.SqlService;
 import com.example.flywood.ekonomka.ui.receipt.ReceiptViewModel;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,12 +31,53 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.flywood.ekonomka.databinding.ActivityMainBinding;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     SqlService sqlService;
+
+    private ActivityResultLauncher<String> resultPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    showCamera(0);
+                } else {
+                    Toast.makeText(this, "Not Permission", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    private ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null) {
+            Toast.makeText(this, "Отмена", Toast.LENGTH_SHORT).show();
+        } else {
+            setResult(result.getContents());
+        }
+    });
+
+    private void setResult(String result) {
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showCamera(int cameraId) {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.EAN_13);
+        options.setPrompt("Scann bar code");
+        options.setCameraId(cameraId);
+        options.setBeepEnabled(false);
+
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(true);
+
+        qrCodeLauncher.launch(options);
+
+    }
+
 
 
     @Override
@@ -89,6 +137,24 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void scanBarcode(View view) {
+        checkPermissionAndShowActivity(this);
+
+    }
+
+    private void checkPermissionAndShowActivity(Context context) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED) {
+            showCamera(0);
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "Разрешения на камеру обязвтельны", Toast.LENGTH_SHORT).show();
+        } else {
+            resultPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
     }
 
     public void getBarcode(View view) {
